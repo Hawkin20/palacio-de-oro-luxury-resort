58a62a94a0f53db81e6c5f202f057ae514f5e7fe
Vincent Paul Ecaldre <vincentecaldre25@gmail.com>
2026-05-15 00:58:03 +0800

    Refactor dietary tag icon rendering logic

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -473,10 +473,8 @@ export default function Menu({ userId, isLoggedIn, cart, setCart }: MenuProps) {
                     {item.dietary_tags && item.dietary_tags.length > 0 && (
                       <div className="flex flex-wrap gap-2 mb-4">
                         {item.dietary_tags.map((tag) => {
-  const config = DIETARY_ICONS[tag];
-  if (!config) return null;
-  const TagIcon = config.icon;
-
+                          const config = DIETARY_ICONS[tag];
+                          if (!config) return null;
                           const TagIcon = config.icon;
                           return (
                             <span 
